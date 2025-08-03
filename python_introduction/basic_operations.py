1202d5d7a2c8d7a02cdd671b91b1044cfd664ee6
Habib Musah <musahhabib004@gmail.com>
2025-08-03 23:05:43 +0000

    Fix: Remove submodule and integrate contents

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -8,4 +8,7 @@ product_result = number1 * number2
 print(f"Addition of {number1} and {number2} is {sum_result}")
 print(f"Subtraction of {number1} and {number2} is {difference_result}")
 print(f"Multiplication of {number1} and {number2} is {product_result}")
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0fbe90e (Fix: Remove submodule and integrate contents)
