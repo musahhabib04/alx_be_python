eec5365772e4493324ee60d09ff364d57a14f5ff
Habib Musah <musahhabib004@gmail.com>
2025-08-18 23:55:03 +0000

    Move MySQLServer.py to correct folder

---
+++ MySQLServer.py
@@ -0,0 +1,29 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    connection = None
+    try:
+        # connect to MySQL server
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",       # replace with your username
+            password="@Habib0208039344"  # replace with your password
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+
+    finally:
+        if connection and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+if __name__ == "__main__":
+    create_database()
